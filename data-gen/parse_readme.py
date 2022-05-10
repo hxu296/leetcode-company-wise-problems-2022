8630d43016b79025e738e73f6d4715e98f811855
huanx <hxu296@wisc.edu>
2022-05-10 13:47:48 -0500

    feat: [experimental] add difficulty and solutions

--- data-gen/parse_readme.py
+++ data-gen/parse_readme.py
@@ -4,15 +4,16 @@ import pandas as pd
 
 parsed_list = []
 df = md_to_df('../data/leetcode_repo_readme.md')
-
 for _, row in df.iterrows():
     name_and_link = row['Title']
+    difficulty = row['Difficulty']
+    solution = row['Solutions'].replace('..', 'https://github.com/fishercoder1534/Leetcode/blob')
     m = re.search(r'\[(.*)\]\((.*)\)', name_and_link)
     name = m[1]
     link = m[2]
-    parsed_list.append((name, link, ))
+    parsed_list.append((name, link, difficulty, solution, ))
 
-parsed_df = pd.DataFrame(parsed_list, columns=['name', 'link'])
+parsed_df = pd.DataFrame(parsed_list, columns=['name', 'link', 'difficulty', 'solution'])
 parsed_df.to_csv('../data/leetcode_problems.csv', index=False)
 
 
